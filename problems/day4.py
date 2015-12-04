86f6456cc5f7addf844c318a32ba9108fdb379ee
Filip Koprivec <jO-Osko@users.noreply.github.com>
2015-12-04 11:24:20 +0100

    Day 4

--- problems/day4.py
+++ problems/day4.py
@@ -6,14 +6,14 @@ from hashlib import md5
 
 def part1(inpu):
     a = 0
-    while not md5( (inpu + str(a)).encode("utf-8"))hexdigest().startswith("00000"):
+    while not md5( (inpu + str(a)).encode("utf-8")).hexdigest().startswith("00000"):
         a += 1
     return a
     
 
 def part2(inpu):
     a = 0
-    while not md5( (inpu + str(a)).encode("utf-8"))hexdigest().startswith("000000"):
+    while not md5( (inpu + str(a)).encode("utf-8")).hexdigest().startswith("000000"):
         a += 1
     return a
     
